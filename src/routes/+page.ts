f6ad108a491f2795b93c35f40ae983ce800907e2
RedCatstone <114140180+RedCatstone@users.noreply.github.com>
2026-03-08 11:47:21 +0100

    fixed redirect

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,7 @@
+import { resolve } from '$app/paths';
 import { redirect } from '@sveltejs/kit';
 
 export function load() {
     // just immidiately yeet the user to /counting
-    redirect(308, `/counting`);
+    redirect(308, resolve('/counting'));
 }
